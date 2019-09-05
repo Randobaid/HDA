956a679d79a94a5c094243f0ced545a45b811b68
Eric Ejimba <ejimba@gmail.com>
2019-09-05 12:17:09 +0300

    Expose API endpoints for Indicator and Target #3

---
+++ HDA.Core/HDA.Core/Controllers/IndicatorsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HDA.Core.ViewModels;
+using HDA.Core.Models.HDACore;
+
+namespace HDA.Core.Controllers
+{
+    [Authorize]
+    public class IndicatorsController : ApiController
+    {
+        private HDACoreContext db = new HDACoreContext();
+
+        [Route("api/indicators")]
+        [HttpGet]
+        public IHttpActionResult Index()
+        {
+            List<IndicatorViewModel> targets = new List<IndicatorViewModel>();
+            DateTimeFormatInfo dateTimeFormatInfo = new DateTimeFormatInfo();
+            foreach(var target in db.Indicators)
+            {
+                targets.Add(new IndicatorViewModel {
+                    IndicatorID = target.IndicatorID,
+                    IndicatorName = target.IndicatorNameEn,
+                });
+            }
+            return Ok(targets);
+        }
+
+        [Route("api/indicators/{id}")]
+        [HttpGet]
+        public IHttpActionResult Details(int id)
+        {
+            List<IndicatorViewModel> targets = new List<IndicatorViewModel>();
+            DateTimeFormatInfo dateTimeFormatInfo = new DateTimeFormatInfo();
+            foreach(var target in db.Indicators.Where(i => i.IndicatorID == id))
+            {
+                targets.Add(new IndicatorViewModel {
+                    IndicatorID = target.IndicatorID,
+                    IndicatorName = target.IndicatorNameEn,
+                });
+            }
+            return Ok(targets);
+        }
+    }
+}
---
+++ HDA.Core/HDA.Core/Controllers/TargetsController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HDA.Core.ViewModels;
+using HDA.Core.Models.HDACore;
+
+namespace HDA.Core.Controllers
+{
+    [Authorize]
+    public class TargetsController : ApiController
+    {
+        private HDACoreContext db = new HDACoreContext();
+
+        [Route("api/targets")]
+        [HttpGet]
+        public IHttpActionResult Index()
+        {
+            List<TargetViewModel> targets = new List<TargetViewModel>();
+            DateTimeFormatInfo dateTimeFormatInfo = new DateTimeFormatInfo();
+            foreach(var target in db.Targets)
+            {
+                targets.Add(new TargetViewModel {
+                    TargetID = target.TargetID,
+                    IndicatorID = target.IndicatorID,
+                    HealthFacilityID = target.HealthFacilityID,
+                    ProviderID = target.ProviderID,
+                    DomainLookupID = target.DomainLookupID,
+                    DirectorateLookupID = target.DirectorateLookupID,
+                    EffectiveDate = target.EffectiveDate,
+                    Year = target.Year,
+                    Month = target.Month,
+                    MonthName = target.Month > 0 ? dateTimeFormatInfo.GetMonthName(target.Month ?? 0) : "",
+                    Value = target.Value,
+                });
+            }
+            return Ok(targets);
+        }
+
+        [Route("api/targets/{id}")]
+        [HttpGet]
+        public IHttpActionResult Details(int id)
+        {
+            List<TargetViewModel> targets = new List<TargetViewModel>();
+            DateTimeFormatInfo dateTimeFormatInfo = new DateTimeFormatInfo();
+            foreach(var target in db.Targets.Where(i => i.TargetID == id))
+            {
+                targets.Add(new TargetViewModel {
+                    TargetID = target.TargetID,
+                    IndicatorID = target.IndicatorID,
+                    HealthFacilityID = target.HealthFacilityID,
+                    ProviderID = target.ProviderID,
+                    DomainLookupID = target.DomainLookupID,
+                    DirectorateLookupID = target.DirectorateLookupID,
+                    EffectiveDate = target.EffectiveDate,
+                    Year = target.Year,
+                    Month = target.Month,
+                    MonthName = target.Month > 0 ? dateTimeFormatInfo.GetMonthName(target.Month ?? 0) : "",
+                    Value = target.Value,
+                });
+            }
+            return Ok(targets);
+        }
+    }
+}
