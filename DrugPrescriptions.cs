63a0e1b79fef1bffce365f0921a9ec9fc2183a53
jongitau <kgitau@iba.co.ke>
2019-09-06 08:00:55 +0300

    DrugPrescriptions.cs
    DrugPrescriptions.cs added to new folder Models\HDACore\Prescriptions to enable pulling of drugs prescribed from new table drugprescriptions

---
+++ DrugPrescriptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HDA.Core.Models.HDACore
+{
+    public class DrugPrescriptions
+    {
+        public int Patientid { get; set; }
+        public int IddrugPrescribed { get; set; }
+        public int Providerid { get; set; }
+        public DateTime PrescriptionDate { get; set; }
+
+    }
+}
