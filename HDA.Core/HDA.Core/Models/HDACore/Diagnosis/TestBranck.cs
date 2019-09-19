5e5e584d4444b85cd42568fc69d92c8ee4b5f6d4
jgitau <jgitau@NG-HISDT-0005>
2019-09-19 22:40:47 +0300

    Testing new Branch

---
+++ HDA.Core/HDA.Core/Models/HDACore/Diagnosis/TestBranck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HDA.Core.Models.HDACore.Diagnosis
+{
+    public class TestBranck
+    {
+        public int Test { get; set; }
+        public string TestString { get; set; }
+    }
+}
