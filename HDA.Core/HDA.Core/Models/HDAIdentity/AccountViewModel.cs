40e1d8ca1ba45ca859f5d96812acac8d33236866
Eric Ejimba <ejimba@gmail.com>
2019-11-13 13:43:06 +0300

    Fix username reference error

--- HDA.Core/HDA.Core/Models/HDAIdentity/AccountViewModel.cs
+++ HDA.Core/HDA.Core/Models/HDAIdentity/AccountViewModel.cs
@@ -69,9 +69,8 @@ namespace HDA.Core.Models.HDAIdentity
         public class LoginViewModel
         {
             [Required]
-            [Display(Name = "User Name")]
-            //[EmailAddress]
-            public string Username { get; set; }
+            [Display(Name = "Username")]
+            public string UserName { get; set; }
 
             [Required]
             [DataType(DataType.Password)]
